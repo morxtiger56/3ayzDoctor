8b5e894bb270279595806d7adb2e64744620220b
Mohamed Salah <80904532+morxtiger56@users.noreply.github.com>
2024-05-14 10:23:15 +0300

    adding mobile side menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,14 +2,15 @@ import { CommonModule } from '@angular/common';
 import { Component, Inject, LOCALE_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faGlobe, faHome, faKitMedical, faPhone, faPlus, faUserDoctor } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faGlobe, faHome, faKitMedical, faPhone, faPlus, faUserDoctor } from '@fortawesome/free-solid-svg-icons';
 import { DropdownModule } from 'primeng/dropdown';
+import { SidebarModule } from 'primeng/sidebar';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FontAwesomeModule, DropdownModule, CommonModule, FormsModule],
+  imports: [FontAwesomeModule, DropdownModule, CommonModule, FormsModule, SidebarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
@@ -19,15 +20,8 @@ export class HeaderComponent {
   constructor(
     @Inject(LOCALE_ID) public activeLocale: string
   ) { }
-
-  locals = [
-    { code: 'en-US', name: 'English' },
-    { code: 'ar', name: 'العربية' }
-  ]
-
-  onChange() {
-    window.location.href = `/${this.activeLocale}`
-  }
+  globeIcon = faGlobe
+  menuIcon = faBars
 
   buttons: {
     label: string,
@@ -55,7 +49,16 @@ export class HeaderComponent {
       }
     ]
 
-  globeIcon = faGlobe
+  locals = [
+    { code: 'en-US', name: 'English' },
+    { code: 'ar', name: 'العربية' }
+  ]
+
+  sidebarVisiable = false
+
+  onChange() {
+    window.location.href = `/${this.activeLocale}`
+  }
 
   changeLanguage(value: string) {
     console.log(value)
